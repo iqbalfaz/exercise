740d6672e9e1ef7ee98c752df7e1f0eea29471ca
iqbalfaz <iqbalfaz.rahman@gmail.com>
2022-11-08 15:06:54 +0700

    Exercise day 1

---
+++ Day1/exercise.js
@@ -0,0 +1,50 @@
+// Write a code to find area of rectangle.
+
+let length = 5;
+let width = 2;
+
+area = length * width
+
+console.log(area);
+
+// Write a code to find perimeter of rectangle.
+
+perimeter = 2 * (length + width);
+
+console.log(perimeter);
+
+// Write a code to find diameter, circumference and area of a circle.
+const pi = 3.14;
+let radius = 5;
+
+diameter = 2 * radius;
+
+console.log(diameter);
+
+circumference = 2 * pi * radius;
+
+console.log(circumference);
+
+areaOfCircle = pi * (radius * radius);
+
+console.log(areaOfCircle);
+
+// Writecode to find angles of triangle if two angles are given.
+let a = 60;
+let b = 60;
+
+thirdAngle = 180 - (a + b);
+
+console.log(thirdAngle);
+
+// Write a code to get difference between dates in days.
+let date1 = new Date("10/10/2022");
+let date2 = new Date("15/10/2022");
+
+let differentTime = date2.getTime() - date1.getTime();
+
+let differentDays = differentTime / (1000 * 3600 * 24);
+
+console.log(differentDays);
+
+// Write a code to convert days to years, months and days.
